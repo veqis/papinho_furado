ecc40235d7e51d34415dcfcb76c3d6acd8f48aea
AlyssonH <61508945+veqis@users.noreply.github.com>
2023-04-12 13:44:41 +0000

    prototipo de versão mais autoral

--- client_gui_customTK.py
+++ client_gui_customTK.py
@@ -3,25 +3,36 @@ import socket
 import tkinter as tk
 import customtkinter as ctk
 
-class Client:
-    self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    self.sock.connect((IPHOST, int(PORTA)))
+class janela_chat:
+    def __init__(self, IPHOST, PORTA):
 
-    msg = ctk.Tk()
-    msg.withdraw()
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.connect((IPHOST, int(PORTA)))
 
-    dialog = login(msg)
-    msg.wait_window(dialog)
+        ################
+        tela_thread = threading.Thread(target=self.interface)
+        comunica_thread = threading.Thread(target=self.recebe_server)
 
-    self.nome = dialog.login
+        tela_thread.start()
+        comunica_thread.start()
+        ###############
 
-    self.gui_completa = False
-    self.executando = True
+    def interface(self):
+        self.janela = ctk.CTk()
+        self.janela.title("Chat")
 
-    tela_thread = threading.Thread(target=self.interface)
-    recebe_server_thread = threading.Thread(target=self.recebe_server)
-    tela_thread.start()
-    recebe_server_thread.start()
+        self.janela.area_chat = ctk.CTkTextbox(self.janela)
+        self.janela.area_chat.grid(row=0, column=1, padx=10, pady=10)
+        self.janela.area_chat.configure(state="disabled")
+
+        self.janela.area_input = ctk.CTkTextbox(self.janela, width=400, height=50)
+        self.janela.area_input.grid(row=1, column=1, padx=10, pady=10)
+
+        self.janela.botao = ctk.CTkButton(self.janela, width=50, height=50, text=">")
+        self.janela.botao.grid(row=1, column=1, sticky="e", padx=5, pady=5)
+
+        self.area_envia = ctk
+
 
 
 
@@ -29,7 +40,6 @@ class janela_nick():
     def __init__(self):
         self.janela = ctk.CTk()
         self.janela.title("Insira seu nome de usuário")
-        self.janela.geometry("160x100")
 
         self.janela.login_input = ctk.CTkEntry(self.janela, placeholder_text="Nome de usuário:")
         self.janela.login_input.grid(row=0, column=1, padx=10, pady=10)
@@ -53,7 +63,6 @@ class dados_server():
     def __init__(self):
         self.janela = ctk.CTk()
         self.janela.title("Dados do servidor")
-        self.janela.geometry("160x150")
 
         self.janela.ip_input = ctk.CTkEntry(self.janela, placeholder_text="IP do servidor:")
         self.janela.ip_input.grid(row=0, column=1, padx=10, pady=10)
@@ -61,7 +70,7 @@ class dados_server():
         self.janela.porta_input = ctk.CTkEntry(self.janela, placeholder_text="Porta do servidor:")
         self.janela.porta_input.grid(row=1, column=1, padx=10, pady=10)
 
-        self.janela.botao_conect = ctk.CTkButton(self.janela, text="Conectar", command=self.connect)
+        self.janela.botao_conect = ctk.CTkButton(self.janela, text="Conectar", command=self.connectar)
         self.janela.botao_conect.grid(row=2, column=0, columnspan=2, padx=10, pady=10)
 
         self.janela.protocol("WM_DELETE_WINDOW", self.ao_fechar)
@@ -69,7 +78,7 @@ class dados_server():
     def ao_fechar(self):
         self.janela.destroy()
 
-    def connect(self):
+    def connectar(self):
         IPHOST = self.janela.ip_input.get()
         PORTA = self.janela.porta_input.get()
 
@@ -78,6 +87,9 @@ class dados_server():
             return
 
         self.janela.destroy()
+
+        client = janela_chat(IPHOST,PORTA)
+
         janela_nick().mostrar()
 
     def mostrar(self):
